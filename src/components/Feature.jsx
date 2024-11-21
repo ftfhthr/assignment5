a614cca844203587540056ff635b57eca5d40b6f
Deng <Toby.Deng@student.tdsb.on.ca>
2024-11-21 13:50:52 -0500

    kinda works?

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -10,22 +10,24 @@ const Feature = () => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        setMovies(axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-                {
-                    headers: {
-                        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDVkNGNkZjdmNTlhOGQyNGY4OGU2N2IwNGMzMDU5YyIsIm5iZiI6MTczMjA0MTEwNy4wOTIwODg1LCJzdWIiOiI2NzNjYmI5MmU3NzJjY2MxNTY0NTNiOGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mn_6h6j8NSLw1cZ4BQFC0BkWlsMJY1fza_EUAyuysaE"
-                    }
+        axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+            {
+                headers: {
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDVkNGNkZjdmNTlhOGQyNGY4OGU2N2IwNGMzMDU5YyIsIm5iZiI6MTczMjA0MTEwNy4wOTIwODg1LCJzdWIiOiI2NzNjYmI5MmU3NzJjY2MxNTY0NTNiOGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mn_6h6j8NSLw1cZ4BQFC0BkWlsMJY1fza_EUAyuysaE"
                 }
-            )
+            }
         )
-        console.log(movies);
+        .then(response => 
+            setMovies(response.data)
+        );
     })
 
     return (
         <div className="feature">
             <h2>Top 3 Movies</h2>
             <div className="movies">
-                <img src={"https://image.tmdb.org/t/p/w500/aosm8NMQ3UyoBVpSxyimorCQykC.jpg"} alt="Hitman movie cover" />
+                <p>{movies}</p>
+                {/* <img src={`https://image.tmdb.org/t/p/w500${movies.results[0].poster_path}`} alt="Hitman movie cover" /> */}
                 <img src={movie2} alt="Jailbreak Love on the Run movie cover" />
                 <img src={movie3} alt="Gifted movie cover" />
             </div>
