18fe11c4c36df1aa5059b73bb3f909a5510137eb
email <toby.deng@student.tdsb.on.ca>
2024-11-22 11:39:54 -0500

    trying stuff

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -7,27 +7,46 @@ import movie3 from "../assets/movie3.jpg"
 import "./Feature.css"
 
 const Feature = () => {
-    const [movies, setMovies] = useState([]);
+    const [movieData, setMovieData] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+    async function getStuff() {
+        const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
             {
                 headers: {
                     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDVkNGNkZjdmNTlhOGQyNGY4OGU2N2IwNGMzMDU5YyIsIm5iZiI6MTczMjA0MTEwNy4wOTIwODg1LCJzdWIiOiI2NzNjYmI5MmU3NzJjY2MxNTY0NTNiOGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mn_6h6j8NSLw1cZ4BQFC0BkWlsMJY1fza_EUAyuysaE"
                 }
             }
         )
-        .then(response => 
-            setMovies(response.data)
-        );
-    })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.error(error));
+        return data;
+    };
+
+    useEffect(() => {
+    //     setMovieData((async () => {
+    //         const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+    //             {
+    //                 headers: {
+    //                     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDVkNGNkZjdmNTlhOGQyNGY4OGU2N2IwNGMzMDU5YyIsIm5iZiI6MTczMjA0MTEwNy4wOTIwODg1LCJzdWIiOiI2NzNjYmI5MmU3NzJjY2MxNTY0NTNiOGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mn_6h6j8NSLw1cZ4BQFC0BkWlsMJY1fza_EUAyuysaE"
+    //                 }
+    //             }
+    //         )
+    //         .then(response => {
+    //             console.log(response);
+    //         })
+    //         .catch(error => console.error(error));
+    //         return data;
+    //     })());
+        console.log(getStuff());
+    }, []);
 
     return (
         <div className="feature">
             <h2>Top 3 Movies</h2>
             <div className="movies">
-                <p>{movies}</p>
-                {/* <img src={`https://image.tmdb.org/t/p/w500${movies.results[0].poster_path}`} alt="Hitman movie cover" /> */}
+                <img src={`https://image.tmdb.org/t/p/w500${getStuff().data.results[0].poster_path}`} alt="Hitman movie cover" />
                 <img src={movie2} alt="Jailbreak Love on the Run movie cover" />
                 <img src={movie3} alt="Gifted movie cover" />
             </div>
