895638789749791d17f1c98881bb32ba968b7649
aperson <therealt0bzyt@gmail.com>
2024-11-25 23:41:36 -0500

    random movies

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,13 +1,14 @@
 import { useRef, useState } from "react"
 import { useEffect } from "react"
 import axios from "axios"
-import movie1 from "../assets/movie1.jpg"
-import movie2 from "../assets/movie2.jpg"
-import movie3 from "../assets/movie3.jpg"
 import "./Feature.css"
 
 const Feature = () => {
     const [movieData, setMovieData] = useState([]);
+    const [movie1, setMovie1] = useState({});
+    const [movie2, setMovie2] = useState({});
+    const [movie3, setMovie3] = useState({});
+    const [done, setDone] = useState(false);
 
     async function getStuff() {
         const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
@@ -18,25 +19,33 @@ const Feature = () => {
             }
         )
         setMovieData(data.data.results);
-        console.log(movieData);
         console.log(data);
+        console.log(movieData);
+        console.log(movieData[Math.floor(Math.random() * movieData.length)]);
+        setMovie1(movieData[Math.floor(Math.random() * movieData.length)]);
+        setMovie2(movieData[Math.floor(Math.random() * movieData.length)]);
+        setMovie3(movieData[Math.floor(Math.random() * movieData.length)]);
+        setDone(true);
+        return data;
     };
-
+    
     useEffect(() => {
         getStuff();
-    });
+    }, [done]);
 
 
     return (
         <div className="feature">
             <h2>Top 3 Movies</h2>
-            <div className="movies">
-                <img src={`https://image.tmdb.org/t/p/w500${movieData[0].poster_path}`} alt="Hitman movie cover" />
-                {/* <img src={`https://image.tmdb.org/t/p/w500/aosm8NMQ3UyoBVpSxyimorCQykC.jpg`} alt="Hitman movie cover" /> */}
-                {/* <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png`} alt="Hitman movie cover" /> */}
-                <img src={movie2} alt="Jailbreak Love on the Run movie cover" />
-                <img src={movie3} alt="Gifted movie cover" />
-            </div>
+                {done === false ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    <div className="movies">
+                        <img src={`https://image.tmdb.org/t/p/w500${movie1?.poster_path}`} alt="Hitman movie cover" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie2?.poster_path}`} alt="Hitman movie cover" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie3?.poster_path}`} alt="Hitman movie cover" />
+                    </div>
+                )}
         </div>
     )
 }
