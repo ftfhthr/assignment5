bc9c4fd7595b49d7cf9d6ed72686ff7f7b45c3a6
aperson <therealt0bzyt@gmail.com>
2024-12-02 01:07:00 -0500

    login works

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -11,19 +11,12 @@ const Feature = () => {
     const [done, setDone] = useState(false);
 
     async function getStuff() {
-        const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-            {
-                headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDVkNGNkZjdmNTlhOGQyNGY4OGU2N2IwNGMzMDU5YyIsIm5iZiI6MTczMjA0MTEwNy4wOTIwODg1LCJzdWIiOiI2NzNjYmI5MmU3NzJjY2MxNTY0NTNiOGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mn_6h6j8NSLw1cZ4BQFC0BkWlsMJY1fza_EUAyuysaE"
-                }
-            }
-        )
+        const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=a05d4cdf7f59a8d24f88e67b04c3059c")
         setMovieData(data.data.results);
         setMovie1(movieData[Math.floor(Math.random() * movieData.length)]);
         setMovie2(movieData[Math.floor(Math.random() * movieData.length)]);
         setMovie3(movieData[Math.floor(Math.random() * movieData.length)]);
         setDone(true);
-        return data;
     };
     
     useEffect(() => {
@@ -34,15 +27,15 @@ const Feature = () => {
     return (
         <div className="feature">
             <h2>Top 3 Movies</h2>
-                {done === false ? (
+                {/* {done === false ? (
                     <h1>Loading...</h1>
-                ) : (
+                ) : ( */}
                     <div className="movies">
-                        <img src={`https://image.tmdb.org/t/p/w500${movie1?.poster_path}`} alt="Hitman movie cover" />
-                        <img src={`https://image.tmdb.org/t/p/w500${movie2?.poster_path}`} alt="Hitman movie cover" />
-                        <img src={`https://image.tmdb.org/t/p/w500${movie3?.poster_path}`} alt="Hitman movie cover" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie1?.poster_path}`} alt="random movie 1" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie2?.poster_path}`} alt="random movie 2" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie3?.poster_path}`} alt="random movie 3" />
                     </div>
-                )}
+                {/* )} */}
         </div>
     )
 }
--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,15 +1,31 @@
 import Header from "../components/Header.jsx"
 import Footer from "../components/Footer.jsx"
+import "./LoginView.css"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const LoginView = () => {
+    const [pass, setPass] = useState("");
+    const navigate = useNavigate();
+
+    const login = (event) => {
+        event.preventDefault();
+        if (pass == "iloveyou") {
+            navigate("/movies");
+        } else {
+            alert("Wrong Password!");
+        }
+    }
+
     return (
         <div>
             <Header />
-            <form action="">
-                <label htmlFor="">email</label>
-                <input type="text" required />
-                <label htmlFor="">password</label>
-                <input type="text" required />
+            <form onSubmit={(e) => { login(e) }}>
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" required />
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" value={pass} onChange={(event) => { setPass( event.target.value ) }} required />
+                <input type="submit" value={"Log In"} required />
             </form>
             <Footer />
         </div>
