733128569dc6a510f4825ea7f44bc162783bac74
aperson <therealt0bzyt@gmail.com>
2024-12-03 09:43:47 -0500

    organize genres

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,7 +1,9 @@
+import "./Genres.css"
+
 const Genres = (props) => {
     console.log(props.genresList);
     return (
-        <div>
+        <div className="genres">
             {props.genresList.map((genre) => (
                 <a href={`/movies/genre/${genre.id}`}>{genre.genre}</a>
             ))}
